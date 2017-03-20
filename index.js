f577e9a978ce4a6ecb30dc1a9f13ce3447885ec8
“aroncey” <artuhrroncey@gmail.comssh-keygen -t rsa -C arthurroncey@gmail.com>
2017-03-20 17:02:46 -0400

    Done.

--- index.js
+++ index.js
@@ -9000,3 +9000,38 @@ const issues = [
     "url": "https://api.github.com/repos/learn-co-curriculum/js-donut-lab/issues/2"
   }
 ];
+
+
+
+var issuesWithUpdatedApiUrl = issues.map(function (issue){
+  var new_url = issue.url.replace('api.github.com', 'api-v2.github.com')
+  return Object.assign({}, issue, {url: new_url})
+})
+
+var comment_count_array = issues.map(function (issue){
+  return issue.comments_count
+})
+
+
+var commentCountAcrossIssues = comment_count_array.reduce((total, count) => {
+    return total + count
+})
+
+var openIssues = issues.filter((issue)=>{
+  return issue.state == "open"
+})
+
+var nonAutomaticIssues = issues.filter((issue)=>{
+  return !issue.body.includes("automatically")
+})
+
+const $tbody = document.getElementById('results');
+
+$tbody.innerHTML = nonAutomaticIssues.map(issue=>
+  `<tr>
+          <td>${issue.body}</td>
+          <td>${issue.created_at}</td>
+          <td>${issue.state}</td>
+  </tr>
+  `
+).join('');
